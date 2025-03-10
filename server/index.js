e85a890db72cfa1013ca67e8e7873dbe96e127c6
kunalsolanki22 <kunalsolanki3333@gmail.com>
2025-03-10 22:56:09 +0530

    cors err resolved

--- server/index.js
+++ server/index.js
@@ -10,7 +10,17 @@ const socket = require("socket.io");
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-app.use(cors());
+const corsOptions = {
+    origin: "https://connecto-chatapp-client.vercel.app", // Allow frontend URL
+    credentials: true, // Allow cookies and auth headers
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions));
+
+
+// app.use(cors());
 app.use(express.json());
 app.use('/api/auth',userRoutes)
 app.use('/api/messages',messageRoutes)
