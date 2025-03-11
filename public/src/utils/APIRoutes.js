9f51be02534a3c8c0c5cdc9c1e1955ef7db5317e
kunalsolanki22 <kunalsolanki3333@gmail.com>
2025-03-11 15:44:23 +0530

    backend url updated

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'https://connecto-chatapp-server.vercel.app';
+export const host = 'https://connecto-chatapp-server.onrender.com'
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
